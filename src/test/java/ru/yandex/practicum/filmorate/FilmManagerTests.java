331a62610a36fd1d032f3f177457afb9e3fa6abf
anton-tt <carduelis@yandex.ru>
2023-06-26 17:32:17 +0700

    Исправление ошибок, выявленных в тестах на GitHub -11

--- src/test/java/ru/yandex/practicum/filmorate/FilmManagerTests.java
+++ src/test/java/ru/yandex/practicum/filmorate/FilmManagerTests.java
@@ -1,18 +1,10 @@
 package ru.yandex.practicum.filmorate;
 
-import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.BeforeEach;
-import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
-import ru.yandex.practicum.filmorate.model.Film;
 import ru.yandex.practicum.filmorate.storage.film.FilmStorage;
 
 import ru.yandex.practicum.filmorate.storage.film.InMemoryFilmStorage;
-import ru.yandex.practicum.filmorate.validation.FilmValidation;
-import java.time.LocalDate;
-import java.util.List;
-import static org.junit.jupiter.api.Assertions.*;
-import static org.junit.jupiter.api.Assertions.assertTrue;
 
 @SpringBootTest
 class FilmManagerTests {
--- src/test/java/ru/yandex/practicum/filmorate/UserManagerTests.java
+++ src/test/java/ru/yandex/practicum/filmorate/UserManagerTests.java
@@ -5,9 +5,6 @@ import org.springframework.boot.test.context.SpringBootTest;
 import ru.yandex.practicum.filmorate.storage.user.InMemoryUserStorage;
 import ru.yandex.practicum.filmorate.storage.user.UserStorage;
 
-
-import static org.junit.jupiter.api.Assertions.assertTrue;
-
 @SpringBootTest
 class UserManagerTests {
     private UserStorage userStorage;
