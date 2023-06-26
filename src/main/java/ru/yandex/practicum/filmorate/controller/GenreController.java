d611a790915d1aa18454658916ccaa551b492b57
anton-tt <carduelis@yandex.ru>
2023-06-26 16:58:53 +0700

    Исправление ошибок, выявленных в тестах на GitHub -1

--- src/main/java/ru/yandex/practicum/filmorate/controller/GenreController.java
+++ src/main/java/ru/yandex/practicum/filmorate/controller/GenreController.java
@@ -6,10 +6,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
-import ru.yandex.practicum.filmorate.model.Film;
 import ru.yandex.practicum.filmorate.model.Genre;
-import ru.yandex.practicum.filmorate.model.Mpa;
-import ru.yandex.practicum.filmorate.service.FilmService;
 import ru.yandex.practicum.filmorate.service.GenreService;
 
 import java.util.List;
